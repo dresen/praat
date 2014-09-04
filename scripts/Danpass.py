4646af26da12ba5379f953780418ae9d2ca10168
ask <ask@login.critt>
2014-09-04 17:42:30 +0200

    Renamed danpass_parser.py to test_parser.py and added more functionality to Danpass.py

--- scripts/Danpass.py
+++ scripts/Danpass.py
@@ -1,25 +1,47 @@
 import codecs
 import os
 import subprocess
-from TextGrid import grid 
+from TextGrid import grid
 from praatparser import parse as gridParse
 
+
 class DanPASS(object):
-	"""docstring for DanPASS"""
-	def __init__(self, corpuspath, outpath=False):
-		super(DanPASS, self).__init__()
-		self.corpuspath = corpuspath
-		self.processedpath = outpath
-		self.loadCorpus()
 
-	def loadCorpus(self, path):
+    """docstring for DanPASS"""
 
-		assert os.path.exists(path) == True
+    def __init__(self, corpuspath, outpath=False):
+        super(DanPASS, self).__init__()
+        self.corpuspath = corpuspath
+        self.processedpath = outpath
+        self.loadCorpus()
+        self.data = {}
+        self.ngrids = 0
 
-		files = os.listdir(path)
-		grids = [x for x in files if os.path.splitext(x)[1] == '.TextGrid']
-		wavs = [x for x in files if os.path.splitext(x)[1] == '.wav']
+    def __getitem__(self, key):
+        """Getter-method for textgrids"""
+        return self.data[key]
 
-		assert len(grids) == len(wavs)
+    def addGrid(self, filehandle, filepath):
+        """Setter-method for textgrids"""
+        g = gridParse(filehandle, filepath)
+        self.data[grid.id] = grid
+        self.ngrids += 1
 
-		
+    def rmGrid(self, grid):
+        """To remove grids from the object"""
+        del self[grid.id]
+        self.ngrids -= 1
+
+    def loadCorpus(self, path):
+        """Loads a preprocessed version of DanPASS. The Textgrids have been converted to utf8 encoding using Praat."""
+        assert os.path.exists(path) == True
+
+        files = os.listdir(path)
+        gridfiles = [x for x in files if os.path.splitext(x)[1] == '.TextGrid']
+        wavs = [x for x in files if os.path.splitext(x)[1] == '.wav']
+        assert len(grids) == len(wavs)
+
+        for f in gridfiles:
+            p = os.path.abspath(f)
+            fh = codecs.open(p, 'r', 'utf8')
+            self.addGrid(fh, p)
---
+++ scripts/test_parser.py
@@ -0,0 +1,65 @@
+from praatparser import parse as gridParse
+import os
+import sys
+import codecs
+import optparse
+
+parser = optparse.OptionParser()
+parser.add_option('-o', '--output',
+                  dest="fout",
+                  default="../dp_mono/m_033_k.processed.TextGrid",
+                  )
+parser.add_option('-s', '--sound',
+                  dest="sound",
+                  action="store",
+                  default='../dp_mono/m_033_k.wav',
+                  )
+parser.add_option('-t', '--table',
+                  dest="soundtype_tbl",
+                  default=False,
+                  action="store",
+                  )
+parser.add_option('-i', '--input',
+                  dest="fin",
+                  default='../dp_mono/m_033_k.TextGrid',
+                  )
+
+options, remainder = parser.parse_args()
+
+print(options)
+
+try:
+    fin = codecs.open(options.fin, 'r', 'utf8')
+    fout = codecs.open(options.fout, 'w', 'utf8')
+    if options.soundtype_tbl:
+        tbl = codecs.open(options.soundtype_tbl, 'r', 'utf8')
+    else:
+        tbl = False
+except:
+    print("Unable to access all necessary files.")
+    sys.exit('Terminate.')
+
+data = gridParse(fin, os.path.abspath(options.fin), tbl)
+
+data.extractTier('"lydskrift"', '"stød-stavelse"', 'ˀ')
+data.extractSegmentTier(['"lydskrift"'], '"stød-kombineret"', 'ˀ')
+#data.extractSegmentTier(['"lydskrift (ord-domæne)"'], '"stød-ord"', 'ˀ')
+
+block = ['"POS"', '"POS (reduceret tagset)"', '"fonemnotation"',
+         '"tryk og tone"', '"fraseintonation"', '"kommentarer"',
+         '"info-struktur"']
+
+data.hnrTier('f0-int-hnr_mono.psc', options.sound, downsample=16)
+#data.intensityTier('scripts/int-hnr_mono.psc', options.sound, downsample=16)
+data.pitchIntTier('f0-int-hnr_mono.psc', options.sound, downsample=16)
+
+# print(data)
+
+#print(data['"Pitch 1"'])
+data.prediction(data['"Harmonicity 2"'], '"stødpred"', '"ˀ"')
+data.printGrid(fout, block)
+
+# TODO:
+#
+
+
